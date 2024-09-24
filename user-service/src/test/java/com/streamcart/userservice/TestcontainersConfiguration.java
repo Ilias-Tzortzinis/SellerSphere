8900fbbe3a4808ad60e97c7b3cac0614535d62ac
ilias <iliastzotzos@gmail.com>
2024-09-24 19:19:36 +0300

    UserService initial commit

---
+++ user-service/src/test/java/com/streamcart/userservice/TestcontainersConfiguration.java
@@ -0,0 +1,8 @@
+package com.streamcart.userservice;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+}
