fc40c595727030fefa5d7dae6ea4e8210aed700b
ilias <iliastzotzos@gmail.com>
2024-09-28 15:50:36 +0300

    user-service initial commit

---
+++ user-service/src/test/java/com/sellersphere/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sellersphere.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
