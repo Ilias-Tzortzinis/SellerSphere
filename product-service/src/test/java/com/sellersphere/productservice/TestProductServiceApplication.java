826818003f0501989bc5058c6a28c4f0bb24fbac
ilias <iliastzotzos@gmail.com>
2024-09-30 15:34:48 +0300

    product-service project structure

---
+++ product-service/src/test/java/com/sellersphere/productservice/TestProductServiceApplication.java
@@ -0,0 +1,11 @@
+package com.sellersphere.productservice;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(ProductServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
